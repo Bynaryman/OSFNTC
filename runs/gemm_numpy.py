fix address calculation in HW for capi3
Former-commit-id: 50b4ff30707870918362e552602fde42cba899db

runs/gemm_numpy.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import numpy as np
+import struct
+def double_to_hex(f):
+    return hex(struct.unpack('<Q', struct.pack('<d', f))[0])
 
 a = np.array([[1, 0], [0, 1]])
 b = np.array([[1, 0], [0, 1]])
@@ -7,8 +10,10 @@ b = np.array([[1, 0], [0, 1]])
 np.matmul(a, b)
 
 m=16
-n=14
-k=10
+n=7
+k=256
+
+np.random.seed(123)
 
 a = np.random.random((m,k))
 b = np.random.random((k,n))
@@ -16,11 +21,16 @@ b = np.random.random((k,n))
 #print(b)
 #print(a[0][0])
 
+print(double_to_hex(b[0][0]))
+print(double_to_hex(b[1][0]))
+print(double_to_hex(b[0][1]))
+print(double_to_hex(b[k-1][n-1]))
+
 C = np.matmul(a, b)
 print(C)
 
-a = np.identity(64)
-b = np.ones((64,28))
-
-C = np.matmul(a, b)
-print(8)
+#a = np.identity(64)
+#b = np.ones((64,28))
+#
+#C = np.matmul(a, b)
+#print(8)