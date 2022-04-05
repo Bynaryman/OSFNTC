871ff3ad74a97c7eccae03bc0c4fe324c4bd4955
Bynaryman <i.f.lledoux@gmail.com>
2022-04-05 13:46:14 +0200

    add a 32bit gemm from numpy
    Former-commit-id: cb0914afd6025e264bbc95070418175ad06346f4

---
+++ runs/gemm_numpy_32.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import numpy as np
+import sys
+import struct
+np.set_printoptions(threshold=sys.maxsize)
+def double_to_hex(f):
+    return hex(struct.unpack('<Q', struct.pack('<d', f))[0])
+
+#a = np.array([[1, 0], [0, 1]])
+#b = np.array([[1, 0], [0, 1]])
+#
+#np.matmul(a, b)
+
+m=65
+n=64
+k=15
+
+np.random.seed(123)
+
+a = np.random.random((m,k)).astype(np.float32)
+b = np.random.random((k,n)).astype(np.float32)
+#print(a)
+#print(b)
+#print(a[0][0])
+
+#print(double_to_hex(b[0][0]))
+#print(double_to_hex(b[1][0]))
+#print(double_to_hex(b[0][1]))
+#print(double_to_hex(b[k-1][n-1]))
+
+C = np.matmul(a, b, dtype=np.float32)
+print(C)
+
+#a = np.identity(64)
+#b = np.ones((64,28))
+#
+#C = np.matmul(a, b)
+#print(8)
