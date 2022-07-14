cdc640adb5d302fb6d6565e391ddd8e6fcbdcd2d
Bynaryman <i.f.lledoux@gmail.com>
2022-07-14 17:39:51 +0200

    update power script
    Former-commit-id: 0964f597e2339095926c0517eff1dc85edd9ce6d

--- misc/script_power.py
+++ misc/script_power.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
+import os
+import time
+cmd_get_volt_ampere = "sudo /home/lledoux/av2r/avr2util-s-4.9.0/avr2util-s -usbcom /dev/ttyACM0 display-sensors grep -E 'ADC00|ADC10' | awk '{print $11}'"
 
 def main():
+	starttime = time.time()
+	seconds = 4.0
+	while True:
+		os.system(cmd_get_volt_ampere)
+	    time.sleep(seconds - ((time.time() - starttime) % seconds))
+
 	pass
 
 if __name__ == '__main__':
