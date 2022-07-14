147006eaf4f3b95dacdd54f75fa1ae68fa19665a
Bynaryman <i.f.lledoux@gmail.com>
2022-07-14 15:33:35 +0000

    minor fix
    Former-commit-id: 818329a280d738ad31a69b1a844c00bab27af194

--- misc/script_power.py
+++ misc/script_power.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import time
-cmd_get_volt_ampere = "sudo /home/lledoux/av2r/avr2util-s-4.9.0/avr2util-s -usbcom /dev/ttyACM0 display-sensors grep -E 'ADC00|ADC10' | awk '{print $11}'"
+cmd_get_volt_ampere = "sudo /home/lledoux/av2r/avr2util-s-4.9.0/avr2util-s -usbcom /dev/ttyACM0 display-sensors | grep -E 'ADC00|ADC10' | awk '{print $11}'"
 
 def main():
 	starttime = time.time()
