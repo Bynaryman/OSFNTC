b9233ea1bda986809dad5a063e00ac86dcfc7ae9
Bynaryman <i.f.lledoux@gmail.com>
2022-07-14 17:53:54 +0200

    improve power script
    Former-commit-id: 19b12269e4371dd1ca1b5349b0d17b4448c7de64

--- misc/script_power.py
+++ misc/script_power.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-import os
+#import os
+import subprocess
 import time
 cmd_get_volt_ampere = "sudo /home/lledoux/av2r/avr2util-s-4.9.0/avr2util-s -usbcom /dev/ttyACM0 display-sensors grep -E 'ADC00|ADC10' | awk '{print $11}'"
 
 def main():
 	starttime = time.time()
-	seconds = 4.0
+	seconds = 1.0
 	while True:
-		os.system(cmd_get_volt_ampere)
+		result = subprocess.check_output(cmd_get_volt_ampere, shell=True, stderr=subprocess.STDOUT)
 		time.sleep(seconds - ((time.time() - starttime) % seconds))
 
 if __name__ == '__main__':
