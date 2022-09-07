5405313ddf93c581522ffab570dbbfa3c5c026d4
Bynaryman <i.f.lledoux@gmail.com>
2022-09-07 18:40:41 +0200

    improved ocaccel template
    Former-commit-id: 03f3829de4bfa5344a9a815302298e3b36680876

--- misc/oc-accel_template_for_bitstream_gen/snap_env.sh
+++ misc/oc-accel_template_for_bitstream_gen/snap_env.sh
@@ -1,4 +1,5 @@
 export ACTION_ROOT=${SNAP_ROOT}/actions/cgemm
 export TIMING_LABLIMIT="-500"
 #export OCSE_ROOT=/home/binaryman/Documents/PhD/high_end/ocse
+export SIMULATION=nosim
 export OCSE_ROOT=${SNAP_ROOT}/../ocse
