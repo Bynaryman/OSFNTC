3b1eb572b32e33d2213357ce63be495014e696c7
Bynaryman <i.f.lledoux@gmail.com>
2022-09-22 16:32:30 +0200

    temp var to fix r-value no addresable
    Former-commit-id: 023bcd589f1f0ceaf11de5f7425392966575f8a7

--- OpenBLAS/backend/sw/gemm_backend.h
+++ OpenBLAS/backend/sw/gemm_backend.h
@@ -201,20 +201,20 @@ static void save_file_from_memory(const char *filename, char **output_data, size
 //typedef unsigned short ushort;
 typedef unsigned int uint;
 
-uint as_uint(const float x) {
+static uint as_uint(const float x) {
     return *(uint*)&x;
 }
-float as_float(const uint x) {
+static float as_float(const uint x) {
     return *(float*)&x;
 }
 
-float half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+static float half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
     const uint e = (x&0x7C00)>>10; // exponent
     const uint m = (x&0x03FF)<<13; // mantissa
     const uint v = as_uint((float)m)>>23; // evil log2 bit hack to count leading zeros in denormalized format
     return as_float((x&0x8000)<<16 | (e!=0)*((e+112)<<23|m) | ((e==0)&(m!=0))*((v-37)<<23|((m<<(150-v))&0x007FE000))); // sign : normalized : denormalized
 }
-uint16_t float_to_half(const float x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+static uint16_t float_to_half(const float x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
     const uint b = as_uint(x)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
     const uint e = (b&0x7F800000)>>23; // exponent
     const uint m = b&0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
