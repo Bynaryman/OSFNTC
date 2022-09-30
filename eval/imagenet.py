28784f9e9f0b2c2607e26bf9ebff8ef6b0619140
Bynaryman <i.f.lledoux@gmail.com>
2022-09-30 09:21:38 +0200

    add cifar100 and test without pin memory
    Former-commit-id: d354dba42780c4d0e0a2c4c3369eb2f56316d1da

--- eval/imagenet.py
+++ eval/imagenet.py
@@ -202,6 +202,25 @@ def main_worker(gpu, ngpus_per_node, args):
 		#trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True, num_workers=args.workers)
         val_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True)
 
+    elif args.data_set=="CIFAR100":
+        #transform_train = transforms.Compose([
+        #    transforms.RandomCrop(32, padding=4),
+        #    transforms.RandomHorizontalFlip(),
+        #    transforms.ToTensor(),
+        #    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        #])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        dataloader = datasets.CIFAR100
+        testset = dataloader(root='./data', train=False, download=True, transform=transform_test)
+        #trainset = dataloader(root='./data', train=True, download=True, transform=transform_train)
+        num_classes = 100
+		#trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True, num_workers=args.workers)
+        val_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+
 
     # create model
     if args.pretrained:
