fix std in normalization and add resnet50
Former-commit-id: 13369d1b496c938e21b2376f977e92a57f1f3e30

eval/eval.py
@@ -201,14 +201,15 @@ def main_worker(gpu, ngpus_per_node, args):
         transform_test = transforms.Compose([
             #transforms.Resize(254),
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2612)),
         ])
         dataloader = datasets.CIFAR10
         testset = dataloader(root='./data', train=False, download=True, transform=transform_test)
         trainset = dataloader(root='./data', train=True, download=True, transform=transform_train)
         num_classes = 10
         train_loader= torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
-        val_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True)
+        val_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True, drop_last=True)
 
     elif args.data_set=="CIFAR100":
         #transform_train = transforms.Compose([