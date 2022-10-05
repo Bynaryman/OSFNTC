136cd7b5af26e38a99c0ba6e1dad9e2b62f64277
Bynaryman <i.f.lledoux@gmail.com>
2022-10-05 16:06:35 +0200

    reput all configs
    Former-commit-id: da4139e47084412b3bc261bc5bea8dd04757a3dd

--- misc/configs.py
+++ misc/configs.py
@@ -86,39 +86,39 @@ configs_proposal1 = [
 ]
 
 configs_proposal2 = [
-		# posits
-        #(200,"posit:4:0"      , 80,  80,  4 , 3, 1,  3 , -4 , 0 ),  # posit<4,0> x approximate
-        #(200,"posit:4:0"      , 80,  80,  4 , 3, 1,  4 , -4 , 3 ),  # posit<4,0> x exact (theory)
-        #(200,"posit:4:0"      , 64,  64,  4 , 3, 1,  6 , -4 , 6 ),  # posit<4,0> x enhanced intuition
+		 posits
+        (200,"posit:4:0"      , 80,  80,  4 , 3, 1,  3 , -4 , 0 ),  # posit<4,0> x approximate
+        (200,"posit:4:0"      , 80,  80,  4 , 3, 1,  4 , -4 , 3 ),  # posit<4,0> x exact (theory)
+        (200,"posit:4:0"      , 64,  64,  4 , 3, 1,  6 , -4 , 6 ),  # posit<4,0> x enhanced intuition
 
-        #(200,"posit:8:2"      ,48 ,  48,  24, 6, 3,   6,  -8, 6 ),  # posit<8,2> x approximate
-        #(200,"posit:8:2"      ,28 ,  28,  24, 6, 3,  48, -48, 31),  # posit<8,2> x exact (quire)
+        (200,"posit:8:2"      ,48 ,  48,  24, 6, 3,   6,  -8, 6 ),  # posit<8,2> x approximate
+        (200,"posit:8:2"      ,28 ,  28,  24, 6, 3,  48, -48, 31),  # posit<8,2> x exact (quire)
 
-        #(200,"posit:16:2"     ,32 ,  31,  56, 7,11,   6, -24, 9 ),  # posit<16,2> x approximate
-        #(200,"posit:16:2"     ,16 ,  16,  56, 7,11, 112,-112,31 ),  # posit<16,2> x exact (quire)
+        (200,"posit:16:2"     ,32 ,  31,  56, 7,11,   6, -24, 9 ),  # posit<16,2> x approximate
+        (200,"posit:16:2"     ,16 ,  16,  56, 7,11, 112,-112,31 ),  # posit<16,2> x exact (quire)
 
-        #(200,"posit:32:2"     ,12 ,  12, 120, 8,27,   6, -48, 9 ),  # posit<32,2> x approximate
-        #(200,"posit:32:2"     ,10 ,  10, 120, 8,27, 240,-240, 31),  # posit<32,2> x exact (quire)
+        (200,"posit:32:2"     ,12 ,  12, 120, 8,27,   6, -48, 9 ),  # posit<32,2> x approximate
+        (200,"posit:32:2"     ,10 ,  10, 120, 8,27, 240,-240, 31),  # posit<32,2> x exact (quire)
 
-        #(200,"posit:64:2"     ,6  ,   6, 248, 9,59,   6,-112, 9 ),  # posit<64,2> x approximate
-        #(200,"posit:64:2"     ,6  ,   6, 248, 9,59, 496,-496, 31),  # posit<64,2> x exact (quire)
+        (200,"posit:64:2"     ,6  ,   6, 248, 9,59,   6,-112, 9 ),  # posit<64,2> x approximate
+        (200,"posit:64:2"     ,6  ,   6, 248, 9,59, 496,-496, 31),  # posit<64,2> x exact (quire)
 
-		## IEEE
-		#(200,"ieee:5:10"      ,32 ,  31,  15, 5,10,   6, -24, 9 ),  # IEEE 754 HP x approximate
-        #(200,"ieee:5:10"      ,28 ,  28,  15, 5,10,  30, -48, 7 ),  # IEEE 754 HP x exact (kulisch)
+		# IEEE
+		(200,"ieee:5:10"      ,32 ,  31,  15, 5,10,   6, -24, 9 ),  # IEEE 754 HP x approximate
+        (200,"ieee:5:10"      ,28 ,  28,  15, 5,10,  30, -48, 7 ),  # IEEE 754 HP x exact (kulisch)
 
-		#(200,"ieee:8:23"      ,16 ,  15, 127, 8,23,   6, -48, 9 ),  # IEEE 754 SP x approximate
-		#(200,"ieee:8:23"      ,10 ,  10, 127, 8,23, 254,-298, 7 ),  # IEEE 754 SP x exact (kulisch)
+		(200,"ieee:8:23"      ,16 ,  15, 127, 8,23,   6, -48, 9 ),  # IEEE 754 SP x approximate
+		(200,"ieee:8:23"      ,10 ,  10, 127, 8,23, 254,-298, 7 ),  # IEEE 754 SP x exact (kulisch)
 
-		#(200,"ieee:11:52"     ,8  ,   7,1023,11,52,   6, -112,9 ),  # IEEE 754 DP x approximate
-        #(200,"ieee:11:52"     ,4  ,   4,1023,11,52,2046,-2148,7 ),  # IEEE 754 DP x exact (kulisch)
+		(200,"ieee:11:52"     ,8  ,   7,1023,11,52,   6, -112,9 ),  # IEEE 754 DP x approximate
+        (200,"ieee:11:52"     ,4  ,   4,1023,11,52,2046,-2148,7 ),  # IEEE 754 DP x exact (kulisch)
 
-		## others
-		#(200,"ieee:4:3"       ,48 ,  48,   7, 4, 3,   6,   -7,6 ),  # FP8 E4M3 x approximate
-		#(200,"ieee:4:3"       ,48 ,  48,   7, 4, 3,  14,  -18,13),  # FP8 E4M3 x exact (kulisch)
+		# others
+		(200,"ieee:4:3"       ,48 ,  48,   7, 4, 3,   6,   -7,6 ),  # FP8 E4M3 x approximate
+		(200,"ieee:4:3"       ,48 ,  48,   7, 4, 3,  14,  -18,13),  # FP8 E4M3 x exact (kulisch)
 
-		#(200,"ieee:5:2"       ,48 ,  48,  15, 5, 2,   6,  -7, 6),  # FP8 E5M2 x approximate
-		#(200,"ieee:5:2"       ,48 ,  48,  15, 5, 2,  30, -40, 5),  # FP8 E5M2 x exact (kulisch)
+		(200,"ieee:5:2"       ,48 ,  48,  15, 5, 2,   6,  -7, 6),  # FP8 E5M2 x approximate
+		(200,"ieee:5:2"       ,48 ,  48,  15, 5, 2,  30, -40, 5),  # FP8 E5M2 x exact (kulisch)
 
         (200,"bfloat16"       ,32 ,  31, 127, 8, 7,   5, -24, 2 ),  # bfloat16 x approximate
         (200,"bfloat16"       ,10 ,  10, 127, 8, 7, 254,-266, 7 ),  # bfloat16 x exact (kulisch)
