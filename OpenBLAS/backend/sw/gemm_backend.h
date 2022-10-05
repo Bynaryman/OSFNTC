bc2d873a7aac7059a7a2f5caf101dbe6a4ac140f
Bynaryman <i.f.lledoux@gmail.com>
2022-10-05 16:27:43 +0200

    add carriage return verbose 3 posit
    Former-commit-id: 3c4fac3da7d03e0a77df8096de7c10b0391971f8

--- OpenBLAS/backend/sw/gemm_backend.h
+++ OpenBLAS/backend/sw/gemm_backend.h
@@ -306,10 +306,10 @@ static char* from_IFLOAT_to_bytes(
 		if (arithmetic_bitwidth == 4) {
 			if (sizeof(IFLOAT)==4) { // from single to posit322
 				double tmp_d = (double)(*arith_in);
-				VERBOSE3(stdout, "incoming float as double: %f", tmp_d);
+				VERBOSE3(stdout, "incoming float as double: %f\n", tmp_d);
 				posit_2_t tmp_px2 = convertDoubleToPX2(tmp_d, 32);
 				uint64_t tmp_ui64 = pX2_int(tmp_px2);
-				VERBOSE3(stdout, "posit32 2 as uint64: %lu", tmp_ui64);
+				VERBOSE3(stdout, "posit32 2 as uint64: %lu\n", tmp_ui64);
 				memcpy(bytes_out, arith_in, 4);
 			}
 			if (sizeof(IFLOAT)==8) { // from double to posit322
