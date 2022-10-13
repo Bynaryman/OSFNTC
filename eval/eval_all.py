dedb80370eed041ad1f8c477a8e3c30fc2ab2cde
Bynaryman <i.f.lledoux@gmail.com>
2022-10-13 16:04:21 +0200

    suppress warning
    Former-commit-id: e1e71153627575806168a3f975350f2850cf9e75

--- eval/eval_all.py
+++ eval/eval_all.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import subprocess
 import re
+import warnings
+warnings.filterwarnings("ignore")
+
 
 import torchvision.models as models_imagenet # these are the default topologies trained with imagenet
 import PyTorch_CIFAR10.cifar10_models as models_cifar10 # these modified nn topologies and pretrained
@@ -21,7 +24,7 @@ for i in model_names_cifar10:
 	print("CIFAR10 x {}".format(i))
 	res = subprocess.check_output(eval_cmd.format(data_set="CIFAR10", model=i), shell=True)
 	val = re.search("RET_VAL:.*", res.decode("utf-8"), re.M)
-	print(val)
+	print(val.group(1))
 
 for i in model_names_imagenet:
 	print("IMAGENET x {}".format(i))
