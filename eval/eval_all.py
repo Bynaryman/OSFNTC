cc392b071d93d471d7182147977881951d48cdf6
Bynaryman <i.f.lledoux@gmail.com>
2022-10-13 11:13:41 +0200

    fix dic name
    Former-commit-id: ce2fb282d70648d9cea7398e7a4f4eeb1bd3ed3c

--- eval/eval_all.py
+++ eval/eval_all.py
@@ -8,18 +8,18 @@ GLIMPSE_IMG_NUMBER = 150
 
 supported_data_sets = ['CIFAR10', 'imagenet']
 
-#model_names_imagenet = sorted(name for name in models_imagenet.__dict__
-#    if name.islower() and not name.startswith("__")
-#    and callable(models_imagenet.__dict__[name]))
-#
-#model_names_cifar10 = sorted(name for name in models_cifar10.__dict__
-#    if name.islower() and not name.startswith("__")
-#    and callable(models_cifar10.__dict__[name]))
+model_names_imagenet = sorted(name for name in models_imagenet.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models_imagenet.__dict__[name]))
+
+model_names_cifar10 = sorted(name for name in models_cifar10.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models_cifar10.__dict__[name]))
 
 eval_cmd = "LD_LIBRARY_PATH=/opt/OpenBLAS/lib/ OMP_NUM_THREADS=1 VERBOSITY=1 python eval.py --evaluate -p 150 --pretrained --data_set {data_set} -a {model} -b 1 --glimpse"
 
-for i in models_imagenet:
+for i in model_names_imagenet:
 	os.system(eval_cmd.format(data_set="imagenet", model=i))
 
-for i in models_cifar10:
+for i in model_names_cifar10:
 	os.system(eval_cmd.format(data_set="CIFAR10", model=i))
