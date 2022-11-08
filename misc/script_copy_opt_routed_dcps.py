dacaf94201abd88d565700e5bac66865e37395ca
Bynaryman <i.f.lledoux@gmail.com>
2022-11-08 10:20:05 +0100

    add script to retrieve all routed opt dcps
    Former-commit-id: 811133a0742aecc09c2eacddc0f19945df6553c7

---
+++ misc/script_copy_opt_routed_dcps.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.append('.')
+from configs import configs
+
+# 1. copy the config c opt_routed.dcp to a folder with the corresponding name
+
+cmd_cp = "cp ./SA_200_{}_{}_{}_{}_m{}_{}_HSSD/hardware/build/Checkpoints/opt_routed_design.dcp ./opt_routed_dcps/SA_200_{}_{}_{}_{}_m{}_{}_HSSD.dcp" # N M arith_in msb lsb ovf
+
+for c in configs:
+	exec_cmd = cmd_cp.format(c[2], c[3], c[1].replace(":","_"), c[7], abs(c[8]), c[9])
+	print(exec_cmd)
+	os.system(exec_cmd)
