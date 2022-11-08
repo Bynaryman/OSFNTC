2563c03d5b9ecd2a8beeb44301dea3fefa9cd096
Bynaryman <i.f.lledoux@gmail.com>
2022-11-08 10:37:49 +0100

    fix tuple script opt dcp
    Former-commit-id: 5261b9cb473a744ab0765535f437b53d1ce494a4

--- misc/script_copy_opt_routed_dcps.py
+++ misc/script_copy_opt_routed_dcps.py
@@ -8,6 +8,6 @@ from configs import configs
 cmd_cp = "cp ./SA_200_{}_{}_{}_{}_m{}_{}_HSSD/hardware/build/Checkpoints/opt_routed_design.dcp ./opt_routed_dcps/SA_200_{}_{}_{}_{}_m{}_{}_HSSD.dcp" # N M arith_in msb lsb ovf
 
 for c in configs:
-	exec_cmd = cmd_cp.format(c[2], c[3], c[1].replace(":","_"), c[7], abs(c[8]), c[9])
+	exec_cmd = cmd_cp.format(c[2], c[3], c[1].replace(":","_"), c[7], abs(c[8]), c[9], c[2], c[3], c[1].replace(":","_"), c[7], abs(c[8]), c[9])
 	print(exec_cmd)
 	os.system(exec_cmd)
